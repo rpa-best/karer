3c30aa7111a0b6aa92bf473d68c86e323a766c6d
x.karimjonov <khtkarimzhonov@gmail.com>
2025-06-03 08:36:24 +0500

    add update_at filter

---
+++ backend/career/filters.py
@@ -0,0 +1,12 @@
+import django_filters.rest_framework as filters
+
+from invoice.models import Order
+
+
+class OrderFilter(filters.FilterSet):
+    updated_at__gte = filters.DateFilter(field_name='updated_at', lookup_expr='gte')
+    updated_at__lte = filters.DateFilter(field_name='updated_at', lookup_expr='lte')
+
+    class Meta:
+        model = Order
+        fields = ['updated_at__gte', 'updated_at__lte']
