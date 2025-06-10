1b62f27e834afbae1ca104935816fe4124873f24
x.karimjonov <khtkarimzhonov@gmail.com>
2025-06-10 12:09:48 +0500

    fixes

--- backend/onec/tasks.py
+++ backend/onec/tasks.py
@@ -1,9 +1,8 @@
 import os
-from django.db import IntegrityError
 import requests
 from django.conf import settings
 from celery import shared_task
-from .models import Organization, Specification, Nomenclature, Price, Balance
+from .models import Organization, Specification, Nomenclature, Price, Balance, Vehicle, Driver
 
 
 HOST = os.getenv('ONEC_HOST', 'http://localhost:8000')
@@ -13,11 +12,13 @@ PASSWORD = os.getenv('ONEC_PASSWORD')
 @shared_task
 def sync_db():
     data = _request()
+    _sync_nomenclatures(data.get('ITEMS', {}))
     _sync_organizations(data.get('ORGANIZATIONS', {}))
     _sync_specifications(data.get('SPECIFICATIONS', {}))
-    _sync_nomenclatures(data.get('ITEMS', {}))
     _sync_prices(data.get('PRICE', {}))
     _sync_balances(data.get('BALANCES', {}))
+    _sync_cars(data.get('VEHICLES', {}))
+    _sync_drivers(data.get('DRIVERS', {}))
 
 
 def _request():
@@ -37,6 +38,7 @@ def _sync_organizations(data):
             'fullname': org_data.get('FULLNAME'),
             'inn': org_data.get('INN'),
             'kpp': org_data.get('KPP'),
+            'do_not_display_volume': org_data.get('DO_NOT_DISPLAY_VOLUME')
         }
         try:
             org = Organization.objects.get(uuid=org_data['XML_ID'])
@@ -52,11 +54,17 @@ def _sync_organizations(data):
 
 def _sync_specifications(data):
     for spec_data in data.values():
+        try:
+            organization = Organization.objects.get(id=spec_data.get('ORGANIZATION_ID'))
+        except organization.DoesNotExist:
+            print("Skipping specification update due to missing organization", spec_data)
+            continue
         defaults={
             'name': spec_data.get('NAME'),
             'delivery_address': spec_data.get('DELIVERY_ADDRESS'),
             'payment_deferment': spec_data.get('PAYMENT_DEFERMENT'),
             'amount_limit': spec_data.get('AMOUNT_LIMIT'),
+            'organization': organization,
         }
         try:
             spec = Specification.objects.get(uuid=spec_data['XML_ID'])
@@ -67,6 +75,12 @@ def _sync_specifications(data):
         except Specification.DoesNotExist:
             spec = Specification.objects.create(uuid=spec_data['XML_ID'], **defaults)
             created = True
+        for item in spec_data.get('ITEMS', {}).values():
+            if spec.nomenclatures.filter(uuid=item).exists():
+                continue
+            nomenclature = Nomenclature.objects.filter(uuid=item).first()
+            if nomenclature:
+                spec.nomenclatures.add(nomenclature)
         print("Created" if created else "Updated", spec)
 
 
@@ -134,3 +148,47 @@ def _sync_balances(data):
             balance = Balance.objects.create(specification=specification, **defaults)
             created = True
         print("Created" if created else "Updated", balance)
+
+
+def _sync_cars(data):
+    for car_data in data.values():
+        defaults={
+            'name': car_data['NAME'],
+            'reg_number': car_data['REG_NUMBER'],
+            'brand': car_data['BRAND'],
+            'our_prorerty': car_data['OUR_PROPERTY'],
+            'trailer_reg_number': car_data['TRAILER_REG_NUMBER'],
+            'trailer_brand': car_data['TRAILER_BRAND']
+        }
+        try:
+            car = Vehicle.objects.get(uuid=car_data['XML_ID'])
+            Vehicle.objects.filter(uuid=car_data['XML_ID']).update(**{
+                key: value for key, value in defaults.items() if value is not None
+            })
+            created = False
+        except Vehicle.DoesNotExist:
+            car = Vehicle.objects.create(uuid=car_data['XML_ID'], **defaults)
+            created = True
+        print("Created" if created else "Updated", car)
+
+
+def _sync_drivers(data):
+    for driver_data in data.values():
+        defaults={
+            'name': driver_data['NAME'],
+            'inn': driver_data['INN'],
+            'phone_number': driver_data['PHONE_NUMBER'],
+            'job_title': driver_data['JOB_TITLE'],
+            'drivers_license_series': driver_data['DRIVERS_LICENSE_SERIES'],
+            'drivers_license_number': driver_data['DRIVERS_LICENSE_NUMBER']
+        }
+        try:
+            driver = Driver.objects.get(uuid=driver_data['XML_ID'])
+            Driver.objects.filter(uuid=driver_data['XML_ID']).update(**{
+                key: value for key, value in defaults.items() if value is not None
+            })
+            created = False
+        except Driver.DoesNotExist:
+            driver = Driver.objects.create(uuid=driver_data['XML_ID'], **defaults)
+            created = True
+        print("Created" if created else "Updated", driver)
